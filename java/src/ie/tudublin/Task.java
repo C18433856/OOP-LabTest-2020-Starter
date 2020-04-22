dfc22b8dbe4fa29754510ae60be3531859909b77
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 13:59:45 +0100

    add private fields and public accessor methods

--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -2,5 +2,37 @@ package ie.tudublin;
 
 public class Task
 {
+    private String Task;
+    private int Start;
+    private int End;
 
+    public String getTask()
+    {
+        return Task;
+    }
+
+    public int getStart()
+    {
+        return Start;
+    }
+
+    public int getEnd()
+    {
+        return End;
+    }
+
+    public void setTask(String Task)
+    {
+        this.Task = Task;
+    }
+
+    public void setStart(int Start)
+    {
+        this.Start = Start;
+    }
+
+    public void setTask(int End)
+    {
+        this.End = End;
+    }
 }
