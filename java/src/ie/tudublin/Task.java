f479bfc9756363ceb20748dd4b2c803f86135e53
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 16:14:48 +0100

    Added constructors

--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -1,10 +1,24 @@
 package ie.tudublin;
 
+import processing.data.TableRow;
+
 public class Task
 {
     private String Task;
     private int Start;
     private int End;
+
+    public Task(String Task, int Start, int End)
+    {
+        this.Task = Task;
+        this.Start = Start;
+        this.End = End;
+    }
+
+    public Task(TableRow tr)
+    {
+        this(tr.getString("Task"), tr.getInt("Start"), tr.getInt("End"));
+    }
 
     public String getTask()
     {
